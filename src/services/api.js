461ccaed1742a61bd970edac17ed2f1adff28194
Javad Bahrami <javadev14bh@gmail.com>
2023-05-12 14:33:00 +0330

    error management in api

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ const BASE_URL = 'https://fakestoreapi.com';
 
 
 export const getProducts = async () => {
-    const response = await axios.get(`${BASE_URL}/products`);
+    const response = await axios.get(`${BASE_URL}/products`)
+    .catch(error => alert(`${error.message} : Please check your connection or try again later`));
     return response.data;
 }
